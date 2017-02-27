0198d5e5a78cd8de38d0f380961ddb7126623922
mikkof1 <mikko.laitinen@student.saimia.fi>
2017-02-27 14:50:21 +0200

    CarObjects added new training code It have few comment's to explain the code

---
+++ carObjects/CarObjects/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarObjects
+{
+    public class Car
+    {
+        private double _speed;
+
+        public Car(string brand)
+        {
+            Brand = brand;
+            _speed = 0;
+        }
+
+        public Car()
+        {
+            _speed = 0;
+        }
+
+        public string Brand { get; } // set; }
+        //     set { _brand = value; } // remove if not needed
+       // }
+
+
+        public double Speed
+        {
+            get { return _speed; }
+         //   set { _speed = value; } // remove if not needed
+        }
+
+        public void Accelerate(double accleration)
+        {
+            _speed = _speed + accleration;
+        }
+
+        public void Break()
+        {
+            _speed = _speed - 10;
+        }
+
+        public override string ToString()
+        {
+         //   return base.ToString();
+            return Print();
+
+        }
+
+        private string Print()
+        {
+            return "Brand: "+ Brand + " , Speed: "+_speed;
+        }
+    }
+}
---
+++ carObjects/CarObjects/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarObjects
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+          //  var myparameter = "10"; // = 10;
+
+            var car = new Car("Volvo");
+            //  car.Brand = "Honda"; // not possible if set removed
+            Console.WriteLine("Brand: " + car.Brand);
+            Console.WriteLine("Speed: " + car.Speed);
+            //  car.Speed = 100; // not possible if set removed
+            car.Accelerate(20);
+            car.Accelerate(10);
+            Console.WriteLine("New speed: " + car.Speed);
+            car.Break();
+            Console.WriteLine("Speed after break: " + car.Speed);
+
+            Console.WriteLine(car.ToString());
+            Console.WriteLine(car); // same as .ToString()
+
+            Console.ReadKey();
+
+            // Car secondCar = new Car();
+
+
+        }
+    }
+}
